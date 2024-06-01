2465e8810fd9602053265699c7ea8833ccb5dc2d
Yesha Pandya <yesha.pandya@outlook.com>
2024-06-01 22:02:21 +0530

    Create 02_variables_and_constants.php

---
+++ 02_variables_and_constants.php
@@ -0,0 +1,7 @@
+// variables are declared using '$' symbol ; for example:
+
+$str="hello";      //string variable
+$num=12;           //numeric variable
+$floating=12.34;   //floating point variable
+$bool=TRUE;        //boolean variable
+
