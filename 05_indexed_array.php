be0f1990d6cfb38fb2b1722802b054ea1eb0c4b2
Yesha Pandya <yesha.pandya@outlook.com>
2024-06-02 12:56:42 +0530

    Update 05_indexed_array.php

--- 05_indexed_array.php
+++ 05_indexed_array.php
@@ -15,5 +15,23 @@
    foreach($cars as $x){
        echo "$x\n";
    }
+   
+   //add array items
+   //To add items to an existing array, you can use the bracket [] syntax
+   $cars[]="honda";
+   
+   //to add multiple items to an existing array, use the array_push() function
+   array_push($cars, "bugatti", "tata", "tesla");
+   
+   //deleting array item
+   //To remove an existing item from an array, you can use the array_splice() function.
+   //With the array_splice() function you specify the index (where to start) and how many items you want to delete.
+   array_splice($cars,1,1);
+   
+   //you can also use the unset() function
+   //NOTE: The unset() function does not re-arrange the indexes, meaning that after deletion the array will no longer contain the missing indexes.
+    unset($cars[1]);
     
+    //array_pop() removes last item
+    //array_shift() removes first item
 ?>
