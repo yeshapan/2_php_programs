5bebcfa3bf57c7a5e70b197a6bd4df6f8504f7e7
Yesha Pandya <yesha.pandya@outlook.com>
2024-06-02 14:20:01 +0530

    Create 08_php_string_functions.php

---
+++ 08_php_string_functions.php
@@ -0,0 +1,24 @@
+//strlen() - returns length of string
+<?php
+    echo strlen("Hello! Welcome to GitHub!");
+?>
+
+//str_word_count() - count words in a string
+<?php
+    echo str_word_count("Hello! Welcome to GitHub!");
+?>
+
+//strrev() - reverse a string
+<?php
+    echo strrev("Hello! Welcome to GitHub!");
+?>
+
+strpos() - search for a text within a string
+<?php
+    echo strpos("Hello! Welcome to GitHub!", "to");
+?>
+
+//str_replace() - replace text within a string
+<?php
+    echo str_replace("Hello! Welcome to GitHub!", "Github", "programming");
+?>
